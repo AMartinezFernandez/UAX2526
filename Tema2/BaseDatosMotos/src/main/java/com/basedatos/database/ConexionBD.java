3fa283d9dbc4ee4d8f800e236972ae58be1057ae
Alvaro Martinez <alvaromartinez@MacBook-Pro-de-Alvaro.local>
2025-11-19 13:22:36 +0100

    1

--- Tema2/BaseDatosMotos/src/main/java/com/basedatos/database/ConexionBD.java
+++ Tema2/BaseDatosMotos/src/main/java/com/basedatos/database/ConexionBD.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConexionBD {
     private static final String URL = "jdbc:mysql://localhost:3306/uax";
     private static final String USER = "root";
-    private static final String PASS = "";
+    private static final String PASS = "root";
 
     public static Connection conectar() {
         try {
